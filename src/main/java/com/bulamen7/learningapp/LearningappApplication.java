6ddb127e7eed94cbb483c4712b5a4489824faee9
Arkadiusz Swioklo <swiokloarkadiusz@gmail.com>
2021-11-18 15:27:55 +0100

    first commit

---
+++ src/main/java/com/bulamen7/learningapp/LearningappApplication.java
@@ -0,0 +1,13 @@
+package com.bulamen7.learningapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bulamen7/learningapp/controller/HelloWorldControllerTest.java
@@ -0,0 +1,35 @@
+package com.bulamen7.learningapp.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class HelloWorldControllerTest {
+    @Autowired
+    HelloWorldController helloWorldController;
+
+    @Test
+    void shouldCheckAnswer() throws Exception {
+        //given
+        String name = "Ala";
+
+        //when
+        String answer = helloWorldController.helloWorld(name);
+
+        //then
+        assertThat(answer).isEqualTo("Hello Ala");
+
+
+
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    @Test
+//    void testHomePage() throws Exception{
+//        mockMvc.perform(get("/hello-world")).andExpect(status().isOk());
+//    }
+    }
+}
